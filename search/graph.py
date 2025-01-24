79f465e2bd49f9e794fc4a8497459f10925db56c
GarrisonAsper <garrison.asper@gmail.com>
2025-01-23 21:56:52 -0800

    hopefully my tests work

--- search/graph.py
+++ search/graph.py
@@ -14,14 +14,62 @@ class Graph:
 
     def bfs(self, start, end=None):
         """
-        TODO: write a method that performs a breadth first traversal and pathfinding on graph G
+    Perform a breadth-first traversal and pathfinding on the graph.
 
-        * If there's no end node input, return a list nodes with the order of BFS traversal
-        * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
-        * If there is an end node input and a path does not exist, return None
+    * If there's no end node input, return a list of nodes in the order of BFS traversal.
+    * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path.
+    * If there is an end node input and a path does not exist, return None.
 
-        """
-        return
+    Args:
+        start (str): The starting node for BFS.
+        end (str, optional): The end node for shortest path search.
+
+    Returns:
+        list or None: BFS traversal order or the shortest path, depending on the input.
+    """
+    # Initialize the queue with the starting node
+        queue = [start]
+        visited_nodes = set()  # Set to track visited nodes
+        parent_node = {start: None}  # Dictionary to store parent nodes
+        traversal_order = []  # List to store traversal order
+
+    # Handle edge cases
+        if len(self.graph.nodes) == 0:
+            raise ValueError('Graph contains no nodes.')
+
+        if start not in self.graph.nodes:
+            raise ValueError('Start node not in Graph.')
+
+        if end is not None and end not in self.graph.nodes:
+            raise ValueError('End node not in Graph.')
+
+    # Perform BFS
+        while queue:
+            current_node = queue.pop(0)  # Dequeue the first node
+            visited_nodes.add(current_node)  # Mark as visited
+            traversal_order.append(current_node)  # Add to traversal order
+
+        # Check if the current node is the end node
+            if current_node == end:
+                path = []
+                while current_node is not None:
+                    path.append(current_node)
+                    current_node = parent_node[current_node]
+                return path[::-1]  # Return the shortest path (reversed)
+
+        # Explore neighbors of the current node
+            for neighbor in self.graph.neighbors(current_node):
+                if neighbor not in visited_nodes and neighbor not in queue:
+                    parent_node[neighbor] = current_node  # Record the parent
+                    queue.append(neighbor)  # Enqueue the neighbor
+
+    # If no end node is specified, return the BFS traversal order
+        if end is None:
+            return traversal_order
+
+    # If an end node is specified but no path exists, return None
+        return None
+            
 
 
 
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -10,6 +10,19 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
+    test_path = 'data/tiny_network.adjlist'
+    test_graph = Graph(test_path)
+
+    test_bfs= test_graph.bfs('Martin Kampmann')
+
+    assert len(test_path) == len(test_graph.graph.nodes)
+
+    correct_path = ['Martin Kampmann','33483487','32790644','31806696','31626775','31540829','Luke Gilbert','Steven Altschuler','Lani Wu','Neil Risch',
+                    'Nevan Krogan','32036252','32042149','30727954','29700475','34272374','32353859','30944313','Hani Goodarzi','Michael McManus',
+                    'Michael Keiser','Atul Butte','Marina Sirota','33232663','32025019','33765435','33242416','31395880','31486345','Charles Chiu']
+    
+    assert test_bfs == correct_path
+    
     pass
 
 def test_bfs():
@@ -23,4 +36,18 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
+
+    long_test_path = 'data/citation_network.adjlist'
+
+    long_graph = Graph(long_test_path)
+
+    none_path = long_graph.bfs('34407489', '34405267')
+
+    assert none_path == None
+
+    correct_shortest_path = ['30814728','Lani Wu','30727954','Michael McManus','29700475','Brian Shoichet']
+    test_shortest_path = long_graph.bfs('30814728', 'Brian Shoichet')
+
+    assert correct_shortest_path == test_shortest_path
+    
     pass
